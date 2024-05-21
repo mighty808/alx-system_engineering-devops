08fc28f9c41987bc19678581fa19ddf72222290b
mighty808 <effahaboagye888@gmail.com>
2024-05-21 11:59:16 +0000

    Added

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-"""Exports to-do list information of all employees to JSON format."""
+""" Script to create dictionary of dict from an api
+"""
 import json
 import requests
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/users"
+    users = requests.get(url).json()
+
+    data_dict = {}
+
+    for user in users:
+        user_id = user.get("id")
+        data_dict[user_id] = []
+        urll = "https://jsonplaceholder.typicode.com/todos/"
+        todos = requests.get(urll+'?userId={}'.format(user_id)).json()
+        for todo in todos:
+            new_dict = {
+                    "username": user.get("username"),
+                    "task": todo.get("title"),
+                    "completed": todo.get("completed")
+                    }
+            data_dict[user_id].append(new_dict)
+
+    with open("todo_all_employees.json", "w") as jfile:
+        json.dump(data_dict, jfile)
